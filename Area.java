79d7aaa26dd211a1bb850f891e31cd480d2db2d5
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-01-31 15:00:34 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,10 @@
+public class Area {
+    public static void main(String[] args) {
+        int r=5,s=10,b;
+        double a;
+        a=3.14*r*r;
+        b=s*s;
+        System.out.println("the area of the circle:"+a);
+        System.out.println("the area of the square:"+b);
+    }
+}
