5225b0d488100a62e200d2226657bf5045fdac31
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-01-31 14:20:56 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test {
+    public static void main(String[] args) {
+        int a=20,b=30,c,d,e;
+        c=a+b;
+        d=b-a;
+        e=a*b;
+        System.out.println("the sum:"+c);
+        System.out.println(d);
+        System.out.println("the product:"+e);
+    }
+}
